e437706d444eb95937555ca6054fd68a04e84b05
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2023-05-28 15:55:56 +0200

    ups

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import './css/new.css';
+import './css/view.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './fetchCountries.js';
 import debounce from 'lodash.debounce';
@@ -10,16 +10,16 @@ const searchBox = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-const renderCountryList = (countries) => {
+const renderCountryList = countries => {
   countryList.innerHTML = '';
-  countries.forEach((country) => {
+  countries.forEach(country => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `<img src="${country.flags.svg}" alt="Flag of ${country.name.common}" width="50"> ${country.name.common}`;
     countryList.appendChild(listItem);
   });
 };
 
-const renderCountryInfo = (country) => {
+const renderCountryInfo = country => {
   const languages = Object.values(country.languages).join(', ');
   countryInfo.innerHTML = `
     <img src="${country.flags.svg}" alt="Flag of ${country.name.common}" width="100">
@@ -30,7 +30,7 @@ const renderCountryInfo = (country) => {
   `;
 };
 
-const onInput = debounce((event) => {
+const onInput = debounce(event => {
   const searchTerm = event.target.value.trim();
   if (searchTerm === '') {
     countryList.innerHTML = '';
@@ -38,9 +38,11 @@ const onInput = debounce((event) => {
     return;
   }
   fetchCountries(searchTerm)
-    .then((countries) => {
+    .then(countries => {
       if (countries.length > 10) {
-        Notify.warning('Too many matches found. Please enter a more specific name.');
+        Notify.warning(
+          'Too many matches found. Please enter a more specific name.'
+        );
       } else if (countries.length > 1) {
         renderCountryList(countries);
         countryInfo.innerHTML = '';
@@ -49,7 +51,7 @@ const onInput = debounce((event) => {
         countryList.innerHTML = '';
       }
     })
-    .catch((error) => {
+    .catch(error => {
       Notify.failure('Oops, there is no country with that name');
       countryList.innerHTML = '';
       countryInfo.innerHTML = '';
